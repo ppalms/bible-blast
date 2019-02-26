be2d0157f461c38eac0c44f2c3c87f5c007392ca
Patrick <pjittles@gmail.com>
2019-02-25 20:51:12 -0600

    Remove old site from repo

--- BibleBlastSite/wwwroot/Parents/YearAwards.aspx.vb
+++
@@ -1,9 +0,0 @@
-﻿
-Partial Class Awards
-    Inherits System.Web.UI.Page
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        If Not My.User.IsAuthenticated Then
-            Response.Redirect("~/login.aspx", True)
-        End If
-    End Sub
-End Class
