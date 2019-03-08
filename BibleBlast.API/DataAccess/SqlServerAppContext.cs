f9b6e644f78662141de0bcf3892e62035d3ef0b4
Patrick Palmer <ppalmer@pm.me>
2019-03-08 16:25:20 -0600

    Remove commented data context code

--- BibleBlast.API/DataAccess/SqlServerAppContext.cs
+++ BibleBlast.API/DataAccess/SqlServerAppContext.cs
@@ -11,29 +11,12 @@ namespace BibleBlast.API.DataAccess
         public SqlServerAppContext(DbContextOptions<SqlServerAppContext> options) : base(options) { }
 
         public DbSet<Kid> Kids { get; set; }
-        // public DbSet<Parent> Parents { get; set; }
-        // public DbSet<KidParent> KidParents { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.ApplySingularTableNameConvention();
-
-            // modelBuilder.Entity<KidParent>()
-            //     .HasKey(x => new { x.Kid, x.ParentId });
-
-            // modelBuilder.Entity<KidParent>()
-            //     .HasOne(x => x.Kid)
-            //     .WithMany(x => x.Parents)
-            //     .HasForeignKey(x => x.KidId)
-            //     .OnDelete(DeleteBehavior.Restrict);
-
-            // modelBuilder.Entity<KidParent>()
-            //     .HasOne(x => x.Parent)
-            //     .WithMany(x => x.Kids)
-            //     .HasForeignKey(x => x.ParentId)
-            //     .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
