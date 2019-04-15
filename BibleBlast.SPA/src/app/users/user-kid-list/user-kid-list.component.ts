d1c9b9a3a81eee8d0ed3987ab82d5d0260cba77d
Patrick Palmer <ppalmer@pm.me>
2019-04-15 16:50:00 -0500

    Add search kids functionality to kid list component

--- BibleBlast.SPA/src/app/users/user-kid-list/user-kid-list.component.ts
+++ BibleBlast.SPA/src/app/users/user-kid-list/user-kid-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Kid } from 'src/app/_models/kid';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap';
 import { KidEditComponent } from 'src/app/kids/kid-edit/kid-edit.component';
 import { KidService } from 'src/app/_services/kid.service';
 import { User } from 'src/app/_models/user';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { KidSearchComponent } from 'src/app/kids/kid-search/kid-search.component';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-user-kid-list',
@@ -15,14 +17,16 @@ export class UserKidListComponent implements OnInit {
   @Input() user: User;
   bsModalRef: BsModalRef;
 
-  constructor(private kidService: KidService, private modalService: BsModalService, private alertify: AlertifyService) { }
+  constructor(
+    private kidService: KidService, private userService: UserService,
+    private modalService: BsModalService, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.sortKids();
   }
 
   // todo get rid of 'any'
-  openKidEdit(kid: any) {
+  openEdit(kid: any) {
     if (!kid.id) {
       kid.organizationId = this.user.organization.id;
       kid.parents = [{ id: this.user.id }];
@@ -48,6 +52,16 @@ export class UserKidListComponent implements OnInit {
     this.bsModalRef.content.kidDeleted.subscribe((id: number) => {
       this.kidService.deleteKid(id).subscribe(() => this.user.kids.splice(this.user.kids.findIndex(x => x.id === id), 1),
         this.alertify.error, () => this.alertify.success('Successfully deleted kid'));
+    });
+  }
+
+  openSearch() {
+    const initialState = { selectedKids: this.user.kids };
+    this.bsModalRef = this.modalService.show(KidSearchComponent, { initialState });
+    this.bsModalRef.content.kidsSelected.subscribe(() => {
+      this.userService.updateUser(this.user).subscribe(() => {
+        this.alertify.success('Successfully updated kids');
+      }, this.alertify.error, () => this.sortKids());
     });
   }
 
