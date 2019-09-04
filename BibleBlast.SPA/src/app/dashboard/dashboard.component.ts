aa5d890cdfb21610d9593a5e117313883802f76e
Patrick Palmer <ppalmer@pm.me>
2019-09-04 13:12:44 -0500

    Add sorting of categories in dashboard

--- BibleBlast.SPA/src/app/dashboard/dashboard.component.ts
+++ BibleBlast.SPA/src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 import { KidService } from '../_services/kid.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { KidMemoryCategory } from '../_models/memory';
+import { DashboardViewModel } from '../_models/kid';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +12,7 @@ import { AlertifyService } from '../_services/alertify.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  viewModel: any[];
+  viewModel: DashboardViewModel[];
   queryParams: any = {
     fromDate: new Date(new Date().setHours(0, 0, 0, 0)),
     toDate: new Date(new Date().setHours(23, 59, 59, 999)),
@@ -28,7 +30,29 @@ export class DashboardComponent implements OnInit {
   loadCompletedMemories() {
     this.kidService.getKidMemories(this.queryParams)
       .subscribe(viewModel => {
+        viewModel.forEach(x => x.categories.sort(this.sortCategories));
         this.viewModel = viewModel;
       }, this.alertify.error);
+  }
+
+  sortCategories = (a: KidMemoryCategory, b: KidMemoryCategory) => {
+    const nameA = a.categoryName.toUpperCase();
+    const nameB = b.categoryName.toUpperCase();
+    const numA: number = parseInt(nameA, 10);
+    const numB: number = parseInt(nameB, 10);
+
+    if (numA && numB) {
+      return numA < numB ? -1 : 1;
+    }
+
+    if (nameA < nameB) {
+      return -1;
+    }
+
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    return 0;
   }
 }
